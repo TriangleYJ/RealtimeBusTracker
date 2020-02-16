ff1d1134ac7bbe6fcb33c2fbf62caaa2faf868eb
triangleyj <dpwns99@gmail.com>
2020-02-16 22:31:58 +0900

    Basic tracker added. (Testing)

--- src/server/App.js
+++ src/server/App.js
@@ -7,8 +7,15 @@ const request = require('request');
 const port_num = process.env.PORT || 80;
 let server_timer = 0;
 
+const url_station = 'http://bis.mokpo.go.kr/mp/bis/searchBusStopRoute.do';
+
 app.set('view engine', 'pug');
 app.set('views', 'routes');
+
+
+function getOpt(url, method, form){
+    return {url, method, form};
+}
 
 //server time counter
 setInterval(() => {
@@ -28,10 +35,33 @@ io.on('connection', socket => {
         console.log('Client connected : ' + socket.id);
     });
 
-    socket.on('test_emit', data=>{
-        this.socket = setInterval(() => {
-            socket.emit('test_emit_res', server_timer);
-        }, 1000);
+    socket.on('test_emit', data =>{
+        socket.my_vehicles_sev = [];
+        if(data != null) {
+            this.interval = setInterval(() => {
+                let data_obj = JSON.parse(data);
+
+                for(let i in data_obj){
+                    let opt = getOpt(url_station, 'POST', {busStopId: data_obj[i]["stopId"]});
+                    //TODO:minimize communication
+                    request.post(opt, (err, res, body) => {
+                        let vehicles = JSON.parse(body)['busStopRouteList'];
+                        let my_vehicles_one = vehicles.filter(e => e["route_id"] === data_obj[i]["routeId"]);
+
+                        let {veh_id, provide_code, provide_type, last_stop_name, rstop, route_name} = my_vehicles_one[0];
+                        let my_vehicle = {veh_id, provide_code, provide_type, last_stop_name, rstop, route_name};
+                        let my_vehicle_string = JSON.stringify(my_vehicle);
+
+                        //console.log(my_vehicle_string);
+
+                        if(my_vehicle_string !== socket.my_vehicles_sev[i] || socket.my_vehicles_sev[i] === undefined){
+                            socket.my_vehicles_sev[i] = my_vehicle_string;
+                            socket.emit('test_emit_res', JSON.stringify([i, my_vehicle]));
+                        }
+                    });
+                }
+            }, 1000);
+        }
     });
 
     socket.on('forcedisconnect', () => {
@@ -40,6 +70,6 @@ io.on('connection', socket => {
 
     socket.on('disconnect', () => {
         console.log('Client disconnected : ' + socket.id);
-        clearInterval(this.socket);
+        clearInterval(this.interval);
     });
 });
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,11 +1,16 @@
 $(() => {
     let socket = io();
+    let wishList = "{\"random20\":{\"routeId\":\"324000015\",\"stopId\":\"101503003\",\"stopId_dst\":\"100101051\",\"afterId\":null},\"random13\":{\"routeId\":\"324000011\",\"stopId\":\"101503004\",\"stopId_dst\":\"130001112\",\"afterId\":null},\"random60\":{\"routeId\":\"324000021\",\"stopId\":\"101502075\",\"stopId_dst\":\"103001033\",\"afterId\":null},\"random1\":{\"routeId\":\"324000001\",\"stopId\":\"111900032\",\"stopId_dst\":\"103001039\",\"afterId\":\"random13\"}}";
+
     socket.emit("connect_", {});
 
-    socket.emit('test_emit', {});
+    socket.emit('test_emit', wishList);
 
-    socket.on('test_emit_res', data => {
-        $('#log').text(data);
+    socket.on('test_emit_res', data_string => {
+        let raw = JSON.parse(data_string);
+        console.log(raw[0], JSON.stringify(raw[1]));
+
+        //$('#log').text(data);
     });
 
 });
